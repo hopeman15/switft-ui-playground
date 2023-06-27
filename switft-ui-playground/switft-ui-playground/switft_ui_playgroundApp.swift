cd51fb0e7441baed0bda6fef06fd8079daff0bb8
Kyle <[hopeman15@users.noreply.github.com]>
2023-06-27 22:13:07 +0200

    ios app setup

---
+++ switft-ui-playground/switft-ui-playground/switft_ui_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  switft_ui_playgroundApp.swift
+//  switft-ui-playground
+//
+//  Created by Kyle Roe on 27.06.23.
+//
+
+import SwiftUI
+
+@main
+struct switft_ui_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ switft-ui-playground/switft-ui-playgroundUITests/switft_ui_playgroundUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  switft_ui_playgroundUITestsLaunchTests.swift
+//  switft-ui-playgroundUITests
+//
+//  Created by Kyle Roe on 27.06.23.
+//
+
+import XCTest
+
+final class switft_ui_playgroundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
