927e334397ce9741d0cc9e5a1b937113b710e442
Kyle Roe <hopeman15@users.noreply.github.com>
2023-07-09 22:32:50 +0200

    [Tutorial] Creating and Combining Views (#1)
    Co-authored-by: Kyle <[hopeman15@users.noreply.github.com]>

---
+++ switft-ui-playground/switft-ui-playground/ui/components/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  switft-ui-playground
+//
+//  Created by Kyle Roe on 09.07.23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
